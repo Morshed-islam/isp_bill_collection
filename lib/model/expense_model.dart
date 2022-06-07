f5a32dd86a4690d3675efd5d54ce2fa391e23de4
Morshed-islam <morshedislam21@gmail.com>
2022-06-07 17:20:07 +0600

    espense entry foorm

--- lib/model/expense_model.dart
+++ lib/model/expense_model.dart
@@ -1,4 +1,42 @@
-class ExpenseModel{
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class ExpenseModel {
+  String? admin_id;
+  String? expense_amount;
+  String? desc;
+  int? day;
+  int? month;
+  int? year;
+  Timestamp? timestamp;
 
+  ExpenseModel(
+      {this.admin_id,
+      this.expense_amount,
+      this.desc,
+      this.day,
+      this.month,
+      this.year,
+      this.timestamp});
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      'admin_id': admin_id,
+      'expense_amount': expense_amount,
+      'desc': desc,
+      'day': day,
+      'month': month,
+      'year': year,
+      'timestamp': timestamp
+    };
+    return map;
+  }
+
+  factory ExpenseModel.fromMap(Map<String, dynamic> map) => ExpenseModel(
+      admin_id: map['admin_id'],
+      expense_amount: map['expense_amount'],
+      desc: map['desc'],
+      day: map['day'],
+      month: map['month'],
+      year: map['year'],
+      timestamp: map['timestamp']);
 }
--- lib/pages/tab_page/expense_entry.dart
+++ lib/pages/tab_page/expense_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ExpenseEntryPage extends StatefulWidget {
   const ExpenseEntryPage({Key? key}) : super(key: key);
@@ -8,10 +9,140 @@ class ExpenseEntryPage extends StatefulWidget {
 }
 
 class _ExpenseEntryPageState extends State<ExpenseEntryPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _costController = TextEditingController();
+  final _descController = TextEditingController();
+
+  DateTime? dateTime;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: ListView(
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(
+                  height: 10,
+                ),
 
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5),
+                  child: TextFormField(
+                    controller: _costController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'খরচের পরিমান',
+                      prefixIcon: Icon(Icons.balance),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'emptyFieldErrMsg';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 5),
+                  child: TextFormField(
+                    controller: _descController,
+                    keyboardType: TextInputType.name,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'বিস্তারিত লিখুন',
+                      prefixIcon: Icon(Icons.description),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'emptyFieldErrMsg';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: Card(
+                    elevation: 4,
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(1.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          TextButton(
+                            child: const Text('এখানে ক্লিক করুন'),
+                            onPressed: _showDatePickerDialog,
+                          ),
+                          Text(dateTime == null
+                              ? 'তারিখ পাওয়া যায়নি!'
+                              : DateFormat('dd/MM/yyyy').format(dateTime!))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: InkWell(
+              child: Center(
+                child: ElevatedButton(
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'সেভ করুন',
+                      style: TextStyle(
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  onPressed: _savaDateToExpenseDb,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _showDatePickerDialog() async{
+    final _selectedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(DateTime.now().year - 1),
+        lastDate: DateTime.now());
+
+    if (_selectedDate != null) {
+      setState(() {
+        dateTime = _selectedDate;
+      });
+    }
+  }
+
+  void _savaDateToExpenseDb() {
+
+    if(_formKey.currentState!.validate()){
+
+    }
+
   }
 }
